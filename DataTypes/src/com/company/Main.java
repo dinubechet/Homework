c3288e379116d777dbbbbad15831754fa8fa7121
Dinu <dinubechet@gmail.com>
2018-08-29 16:48:12 +0300

    New addition dollar conversion and the Lib thing game.

--- DataTypes/src/com/company/Main.java
+++ DataTypes/src/com/company/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
          //printInitials();
         //countTheEggs();
-         moneyMakingHuntington();
+        //moneyMakingHuntington();
+        //convertingDollars();
+       // timeConversion();
+        playMadLibs();
     }
 
     static void printInitials() {
@@ -38,6 +41,48 @@ public class Main {
         int totalMoney = adultMoney+childMoney;
         System.out.println("You ordered adult meals in the sum of "+adultMoney+" $, child meals in the sum of "+childMoney+"$, with a total of "+totalMoney+" $");
 
+    }
+
+    static void convertingDollars (){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a dollar amount: ");
+        int dollarAmount = scan.nextInt();
+
+        int numTwenties = dollarAmount/20;
+        int numTens = (dollarAmount - 20*numTwenties)/10;
+        int numFives = (dollarAmount - (10*numTens + 20*numTwenties))/5;
+        int numOnes = (dollarAmount - (10*numTens + 20*numTwenties + 5*numFives));
+
+        System.out.println("You have entered  $" + dollarAmount );
+
+        System.out.println("That is " + numTwenties + " twenties, " + numTens + " tens, " + numFives + " fives, and " + numOnes + " ones.");
+    }
+
+    static void timeConversion(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter minutes amount : ");
+        int minutes = scan.nextInt();
+        double hour = minutes/60;
+        double day = hour/24;
+        System.out.println("You entered "+minutes+" minutes, and that is "+hour+" hours, and "+day+" days.");
+        System.out.printf("You entered %s minutes, and that is %.2f hours, and %.2f days.",minutes,hour,day);
+    }
+
+    static void playMadLibs(){
+        Scanner scan = new Scanner(System.in);
+
+        String noun, noun2,adjective,pastTenseVerb;
+        System.out.println("Enter a noun ");
+        noun = scan.nextLine();
+        System.out.println("Enter a second noun: ");
+        noun2 = scan.nextLine();
+        System.out.println("Enter an adjective: ");
+        adjective=scan.nextLine();
+        System.out.println("Enter a past-tense verb: ");
+        pastTenseVerb=scan.nextLine();
+        System.out.println("Mary had a little " + noun + "\nIts " + noun2 + " was " + adjective + " as snow");
+        System.out.println("And everywhere that Mary " + pastTenseVerb);
+        System.out.println("The " + noun + " was sure to go.");
     }
 
 }
