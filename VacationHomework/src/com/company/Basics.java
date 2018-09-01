db9254b0c56bc41312627f27b39ab96590be881a
Dinu <dinubechet@gmail.com>
2018-09-01 21:30:45 +0300

    Changed the name to Basics

---
+++ VacationHomework/src/com/company/Basics.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Basics {
+public static void main (String[] args) {
+    //displayTheQuote();
+    //printTheQuoteWithInformationAboutTheMovie();
+    //drawTableWithChairs();
+    //carlysMotto();
+}
+    static void displayTheQuote (){
+        System.out.println("My mama always said, ‘Life was like a box of chocolates. You never know what you’re gonna get.");
+    }
+
+
+    static void printTheQuoteWithInformationAboutTheMovie(){
+        System.out.println("“My mama always said, ‘Life was like a box of chocolates. You never know what you’re gonna get.’” – Forrest Gump");
+        System.out.println(" From the movie 'Forrest Gump', filmed in 1994 by  Robert Zemeckis");
+    }
+
+
+    static void drawTableWithChairs(){
+        System.out.println("X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
+
+    }
+
+
+    static void carlysMotto(){
+        System.out.println("Carly’s makes the food that makes it a party.");
+    }
+}
