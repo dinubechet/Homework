0b88a40b6751fa4117023eab9dded62b5876c0a0
Dinu <dinubechet@gmail.com>
2018-09-05 15:27:39 +0300

    Did the first  exercise with NumbersDemo class.

--- Methods/src/com/company/NumbersDemo.java
+++ Methods/src/com/company/NumbersDemo.java
@@ -8,8 +8,8 @@ public class NumbersDemo {
         int secondInt = 3;
 
         displayTwiceTheNumber(firstInt, secondInt);
-        displayNumberPlusFive(firstInt,secondInt);
-        displayNumberSquared(firstInt,secondInt);
+        displayNumberPlusFive(firstInt, secondInt);
+        displayNumberSquared(firstInt, secondInt);
     }
 
     public static void displayTwiceTheNumber(int num1, int num2) {
@@ -20,20 +20,20 @@ public class NumbersDemo {
 
     public static void displayNumberPlusFive(int numb1, int numb2) {
 
-        int numb1PlusFive = numb1+5;
-        int numb2PlusFive = numb2+5;
+        int numb1PlusFive = numb1 + 5;
+        int numb2PlusFive = numb2 + 5;
 
         System.out.println("The number + 5 = " + numb1PlusFive);
         System.out.println("The number + 5 = " + numb2PlusFive);
     }
 
-    public static void displayNumberSquared(int numbr1, int numbr2){
+    public static void displayNumberSquared(int numbr1, int numbr2) {
 
-        numbr1=numbr1*numbr1;
-        numbr2=numbr2*numbr2;
+        numbr1 = numbr1 * numbr1;
+        numbr2 = numbr2 * numbr2;
 
-        System.out.println("Number's square is = " +numbr1 );
-        System.out.println("Number's square is = " +numbr2 );
+        System.out.println("Number's square is = " + numbr1);
+        System.out.println("Number's square is = " + numbr2);
 
     }
 }
