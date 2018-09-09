19c5544f4a3ae0ed111c3525092bb3b3c0d46255
Dinu <dinubechet@gmail.com>
2018-09-09 22:12:21 +0300

    Exercise number 2 , "The Circle"

---
+++ VacationHomework/Circle/src/Circle.java
@@ -0,0 +1,41 @@
+public class Circle {
+    private double radius;
+    private double diameter;
+    private double area;
+
+    public Circle() {
+        setRadius(1);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+        this.diameter = radius * 2;
+        this.area = (Math.PI * (radius * radius));
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void showInfo(){
+        System.out.println("The radius is : " + getRadius());
+        System.out.println("The diameter is : " + getDiameter());
+        System.out.println("The area is : " + getArea());
+    }
+}
---
+++ VacationHomework/Circle/src/TestCircle.java
@@ -0,0 +1,14 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle();
+        Circle circle3 = new Circle();
+
+        circle1.setRadius(20);
+        circle2.setRadius(40);
+
+        circle1.showInfo();
+        circle2.showInfo();
+        circle3.showInfo();
+    }
+}
