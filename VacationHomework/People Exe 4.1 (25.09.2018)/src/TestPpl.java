4bd226bf98c706065891c5853c92340a7d5bb0f0
Dinu <dinubechet@gmail.com>
2018-09-25 14:55:18 +0300

    Exe 4.1 ( 25.09.2018)

---
+++ VacationHomework/People Exe 4.1 (25.09.2018)/src/TestPpl.java
@@ -0,0 +1,10 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class TestPpl {
+    public static void main(String[] args) {
+        Student student1 = new Student("Dinu", "Ciocana", "Cheaper at night", 2018, 10);
+        System.out.println(student1);
+        Staff staff1 = new Staff("Joric", "Shikago", "Dinu Bechet highest school", 300);
+        System.out.println(staff1);
+    }
+}
