1eb2380cf69f0d3f36b2bc1238a931a5550d5555
Dinu <dinubechet@gmail.com>
2018-10-07 19:58:04 +0300

    Duplicate Elimination exerc

---
+++ VacationHomework/DuplicateElimination/src/Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        DuplicateElimination demo = new DuplicateElimination();
+        demo.checkDupe();
+    }
+}
---
+++ VacationHomework/DuplicateElimination/src/DuplicateElimination.java
@@ -0,0 +1,43 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.*;
+
+public class DuplicateElimination {
+    private int[] inputNumbers = new int[5];
+
+    public void input() {
+        Scanner scan = new Scanner(System.in);
+        int[] finalArray;
+
+        for (int i = 0; i < 5; i++) {
+            int number = scan.nextInt();
+            boolean contains = false;
+            for (int num : inputNumbers) {
+                if (num == number) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (contains == false) {
+                inputNumbers[i] = number;
+            }
+
+        }
+
+        System.out.println(Arrays.toString(inputNumbers));
+    }
+
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    public void checkDupe() {
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            int number = scan.nextInt();
+            if (!numbers.contains(number)){
+                numbers.add(number);
+            }
+        }
+        System.out.println(numbers.toString());
+    }
+}
